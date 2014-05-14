20e912ffc38f1691222e33e44f565bff314fa126
mustafauysal <m_uysl@hotmail.com>
2014-05-15 00:18:08 +0300

    initial commit

---
+++ readme.txt
@@ -0,0 +1,26 @@
+=== SOMA Ribbon ===
+Contributors: m_uysl
+Tags: soma
+Requires at Least: 3.0
+Tested Up To: 3.9.1
+Stable tag: 1.0
+
+Turkey mourns victims of tragic mine blast.
+
+== Description ==
+
+This plugin adds black ribbon for our losts.
+
+== Installation ==
+
+1. Download the plugin zip archive.
+2. Extract it in your `wp-content/plugins` folder or upload via admin panel.
+3. Browse to your plugins section and active the plugin.
+
+== Screenshots ==
+1. Here's how the ribbon will appear on your website.
+
+== Changelog ==
+
+= 1.0 =
+* Initial release
---
+++ soma-ribbon.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Plugin Name: SOMA Ribbon
+Plugin URI: https://github.com/mustafauysal/soma-ribbon
+Description: This plugin will put a black ribbon on the bottom right corner of your website.
+Author: Mustafa UYSAL
+Version: 1.0
+License: GPLv2
+Author URI: http://uysalmustafa.com/
+*/
+
+function komur_ribbon() {
+	echo '<img src='.plugins_url( 'black-ribbon.png', __FILE__ ).' alt="Bir avuç kömür için ömür verenlere..."  title="Bir avuç kömür için ömür verenlere..." style="position: fixed; bottom: 0; right: 0; z-index: 999999; cursor: pointer;" />';
+}
+
+add_action( 'wp_footer', 'komur_ribbon' );
